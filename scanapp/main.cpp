1594fd99ed7f0ec94066cb6b865844ed80887c4e
Frederic Kehrein <frederic@kehrein.org>
2025-02-10 23:10:35 +0100

    Fixing buildsystem

--- scanapp/main.cpp
+++ scanapp/main.cpp
@@ -1,3 +1,35 @@
+#include "sl_lidar.h" 
+#include "sl_lidar_driver.h"
+
+#include <iostream>
+#include <memory>
+
+using namespace sl;
+
+constexpr auto SERIAL_PORT = "/dev/ttyUSB0";
+
 int main(int argc, char const **argv) {
-  return 0;
+    Result<IChannel *> chopt = createSerialPortChannel(SERIAL_PORT, 115200);
+    if (!chopt) {
+      std::cerr << "Error operaning serial port " << SERIAL_PORT << std::endl;
+      return 1;
+    }
+    std::unique_ptr<IChannel> channel(*chopt);
+    std::unique_ptr<ILidarDriver> lidar(*createLidarDriver());
+    auto res = lidar->connect(channel.get());
+    if (SL_IS_OK(res)) {
+        sl_lidar_response_device_info_t deviceInfo;
+        res = lidar->getDeviceInfo(deviceInfo);
+        if(SL_IS_OK(res)){
+            std::cout << "Model: " << deviceInfo.model 
+                      << "Firmware Version: " << (deviceInfo.firmware_version >> 8) << "." << (deviceInfo.firmware_version & 0xffu)
+                      << std::endl;
+        } else{
+            std::cerr << "Failed to get device information from LIDAR " << res << std::endl;
+        }
+    } else {
+        std::cerr << "Failed to connect to LIDAR " << res << std::endl;
+    }
+    
+    return 0;
 }
